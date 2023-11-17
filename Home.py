fee4631bbe8a8eb668802dd40edd18eb83fb4bbc
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-11-17 22:19:54 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,9 +18,9 @@ st.subheader("Nonthakan Jarpun 🐻 DATA SCIENCE NPRU")
 col1, col2 = st.columns(2)
 with col1:
     st.image('./pic/earth.jpg')
-    lot3="https://lottie.host/ab6efbe2-81bd-4dd5-a2a8-e9567db3ae42/Exp97i4ktM.json"
-    lottie3 = load_lottieurl(lot3)
-    st_lottie(lottie3)
+    #lot3="https://lottie.host/ab6efbe2-81bd-4dd5-a2a8-e9567db3ae42/Exp97i4ktM.json"
+    #lottie3 = load_lottieurl(lot3)
+    #st_lottie(lottie3)
 with col2:
     st.image('./pic/DS1.jpg')
 st.balloons()
